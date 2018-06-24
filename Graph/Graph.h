b39ea1ac4114e3a10b82b253fd2f0385bbe51042
vsc-hvdc <893144519@qq.com>
2018-06-24 16:04:30 +0800

    version2.0
    rewrite the graph.h & test file

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -1,169 +1,312 @@
 #pragma once
-
-/*
-*	@author: ZhouHang
-*	data structure project
-*	adjacency list graph and prim alg
-*/
-
 #include<iostream>
 #include<vector>
 
+struct ALNode
+{
+	int dest;
+	int weight;
+	struct ALNode * next;
+};
+
+struct AL
+{
+	ALNode * head;
+};
+
+struct ALGraph
+{
+	int vertexSize; // number of vertex, aka size of Adj List
+	AL * array; 
+};
+
+
 /*
-*	edge info
-*
+*	@decription: toolkit for construct new node
 */
-template<typename T, typename E>
-struct EdgeNode
+ALNode * newALNode(int dest, int weight)
 {
-	int id;
-	E weight;
-	EdgeNode<T, E> * next;
-	EdgeNode() {};
-};
+	ALNode * newNode = (ALNode*)malloc(sizeof(ALNode));
+	newNode->dest = dest;
+	newNode->weight = weight;
+	newNode->next = NULL;
+	return newNode;
+}
 
 /*
-*	vertex info
-*
+*	@decription:创建一个只有节点没有边的图
 */
-template<typename T, typename E>
-struct Vertex
+ALGraph * creatGraph(int v)
 {
-	T data;
-	//int id;
-	EdgeNode<T, E> * adjList;
-	Vertex(T Data) : data(Data), adjList(NULL) {};
-};
+	ALGraph * graph = (ALGraph *)malloc(sizeof(ALGraph));
+	AL * newArray = (AL *)malloc(sizeof(AL));
 
-template<typename T, typename E>
-class ALGraph
-{
-	//Vertex<T, E> * nodeTable;
-	std::vector<Vertex<T, E>> nodeTable;
-	int vertexNum;
-public:
-	ALGraph() {};
-	bool insertEdge(int, int, E);
-
-	//to do
-	//insertVertex()
-	//removeVertex()
-	//removeEdge()
-public:
-	/*
-	*	@Brief:			file data interface
-	*	@Param:			file path & 2 empty vector used to contain vertex and edge info
-	*	@Description:	interface for client test.
-	*/
-	bool readFile(std::string, std::vector<T> &, std::vector<std::array<std::string, 3>> &);
-	
-	/*
-	*	@Brief:			build the graph
-	*	@Param:			2 vectors contain vertex and edge info
-	*	@Description:
-	*/
-	bool buildGraph(std::vector<T>, std::vector<std::array<std::string, 3>>);
-
-	bool displayBFS();
-
-	/*
-	*	@Brief: build the minist spanning tree using Prim alg
-	*	@Description: start the tree by 
-	*	@Tip: This function only changes the edges and show them
-	*	by BFS. It doesn't build a real tree by definition.
-	*/
-	bool primMst(int id);
-};
-
-
-template<typename T, typename E>
-bool ALGraph<T, E>::insertEdge(int id1, int id2, E Weight)
-{
-	if (id1 >= 0 && id1 <= vertexNum && id2 >= 0 && id2 <= vertexNum)
+	graph->vertexSize = v;
+	graph->array = newArray;
+	for (int i = 0; i < v; i++)
 	{
-		//nodeTable[id1]无法访问成员
-		EdgeNode<T, E> *q, *p = nodeTable[id1].adjList;
-		while (p != NULL && p->id != id2) p = p->next;
+		graph->array[i].head = NULL;
+	}
+	return graph;
+}
 
-		//if edge already exits, return false
-		if (p != NULL) return false;
+/*
+*	@description:在图G中节点dest1, dest2之间加上一条边 
+*	@param: index of node->dest1,dest2
+*/
+void addEdge(ALGraph * G, int dest1, int weight, int dest2)
+{
+	ALNode * newNode = (ALNode*)malloc(sizeof(ALNode));
+	
+	//step1. 在dest2节点的array头插入dest1节点
+	newNode = newALNode(dest1, weight);
+	newNode->next = G->array[dest2].head;
+	G->array[dest2].head = newNode;
 
-		//else insert the edge
-		//insert nodes to the first node of both vertexs
-		p = new EdgeNode<T, E>; 
-		p->id = id2;
-		p->weight = Weight;
-		p->next = nodeTable[id1].adjList;
-		nodeTable[id1].adjList = p;
+	//step2. 在dest1节点的array头里插入dest2节点
+	newNode = newALNode(dest2, weight);
+	newNode->next = G->array[dest1].head;
+	G->array[dest1].head = newNode;
+}
 
-		q = new EdgeNode<T, E>;
-		q->id = id1;
-		q->weight = Weight;
-		q->next = nodeTable[id2].adjList;
-		nodeTable[id2].adjList = q;
-		
-		vertexNum++;
+/**************************************/
+
+// Structure to represent a min heap node
+struct MinHeapNode
+{
+	int  v;
+	int key;
+};
+
+// Structure to represent a min heap
+struct MinHeap
+{
+	int size;      // Number of heap nodes present currently
+	int capacity;  // Capacity of min heap
+	int *pos;     // This is needed for decreaseKey()
+	struct MinHeapNode **array;
+};
+
+// A utility function to create a new Min Heap Node
+struct MinHeapNode* newMinHeapNode(int v, int key)
+{
+	struct MinHeapNode* minHeapNode =
+		(struct MinHeapNode*) malloc(sizeof(struct MinHeapNode));
+	minHeapNode->v = v;
+	minHeapNode->key = key;
+	return minHeapNode;
+}
+
+// A utilit function to create a Min Heap
+struct MinHeap* createMinHeap(int capacity)
+{
+	struct MinHeap* minHeap =
+		(struct MinHeap*) malloc(sizeof(struct MinHeap));
+	minHeap->pos = (int *)malloc(capacity * sizeof(int));
+	minHeap->size = 0;
+	minHeap->capacity = capacity;
+	minHeap->array =
+		(struct MinHeapNode**) malloc(capacity * sizeof(struct MinHeapNode*));
+	return minHeap;
+}
+
+// A utility function to swap two nodes of min heap. Needed for min heapify
+void swapMinHeapNode(struct MinHeapNode** a, struct MinHeapNode** b)
+{
+	struct MinHeapNode* t = *a;
+	*a = *b;
+	*b = t;
+}
+
+// A standard function to heapify at given idx
+// This function also updates position of nodes when they are swapped.
+// Position is needed for decreaseKey()
+void minHeapify(struct MinHeap* minHeap, int idx)
+{
+	int smallest, left, right;
+	smallest = idx;
+	left = 2 * idx + 1;
+	right = 2 * idx + 2;
+
+	if (left < minHeap->size &&
+		minHeap->array[left]->key < minHeap->array[smallest]->key)
+		smallest = left;
+
+	if (right < minHeap->size &&
+		minHeap->array[right]->key < minHeap->array[smallest]->key)
+		smallest = right;
+
+	if (smallest != idx)
+	{
+		// The nodes to be swapped in min heap
+		MinHeapNode *smallestNode = minHeap->array[smallest];
+		MinHeapNode *idxNode = minHeap->array[idx];
+
+		// Swap positions
+		minHeap->pos[smallestNode->v] = idx;
+		minHeap->pos[idxNode->v] = smallest;
+
+		// Swap nodes
+		swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
+
+		minHeapify(minHeap, smallest);
+	}
+}
+
+// A utility function to check if the given minHeap is ampty or not
+int isEmpty(struct MinHeap* minHeap)
+{
+	return minHeap->size == 0;
+}
+
+// Standard function to extract minimum node from heap
+struct MinHeapNode* extractMin(struct MinHeap* minHeap)
+{
+	if (isEmpty(minHeap))
+		return NULL;
+
+	// Store the root node
+	struct MinHeapNode* root = minHeap->array[0];
+
+	// Replace root node with last node
+	struct MinHeapNode* lastNode = minHeap->array[minHeap->size - 1];
+	minHeap->array[0] = lastNode;
+
+	// Update position of last node
+	minHeap->pos[root->v] = minHeap->size - 1;
+	minHeap->pos[lastNode->v] = 0;
+
+	// Reduce heap size and heapify root
+	--minHeap->size;
+	minHeapify(minHeap, 0);
+
+	return root;
+}
+
+// Function to decreasy key value of a given vertex v. This function
+// uses pos[] of min heap to get the current index of node in min heap
+void decreaseKey(struct MinHeap* minHeap, int v, int key)
+{
+	// Get the index of v in  heap array
+	int i = minHeap->pos[v];
+
+	// Get the node and update its key value
+	minHeap->array[i]->key = key;
+
+	// Travel up while the complete tree is not hepified.
+	// This is a O(Logn) loop
+	while (i && minHeap->array[i]->key < minHeap->array[(i - 1) / 2]->key)
+	{
+		// Swap this node with its parent
+		minHeap->pos[minHeap->array[i]->v] = (i - 1) / 2;
+		minHeap->pos[minHeap->array[(i - 1) / 2]->v] = i;
+		swapMinHeapNode(&minHeap->array[i], &minHeap->array[(i - 1) / 2]);
+
+		// move to parent index
+		i = (i - 1) / 2;
+	}
+}
+
+// A utility function to check if a given vertex
+// 'v' is in min heap or not
+bool isInMinHeap(struct MinHeap *minHeap, int v)
+{
+	if (minHeap->pos[v] < minHeap->size)
 		return true;
-	}
-	return 0;
+	return false;
 }
 
-template<typename T, typename E>
-bool ALGraph<T, E>::readFile(std::string filename, std::vector<T> & nodes, std::vector<std::array<std::string, 3>> & edges)
+/*********************************************/
+
+std::string encoder(int i)
 {
-	//to do
-	return true;
+	switch (i)
+	{
+	case 0:
+		return "V1";
+	case 1:
+		return "V2";
+	case 2:
+		return "V3";
+	case 3:
+		return "V4";
+	case 4:
+		return "V5";
+	case 5:
+		return "V6";
+	default:
+		break;
+	}
 }
 
-template<typename T, typename E>
-bool ALGraph<T, E>::buildGraph(std::vector<T> nodesDatas, std::vector<std::array<std::string, 3>> edgesInfoArrays)
+void displayMSTInfo(std::vector<int> arr)
 {
-	//step 1 : build the vertex list
-	vertexNum = 0;
-	for (const auto & nodeData : nodesDatas)
+	int weight = 0;
+	for (int i = 0; i < arr.size(); i++)
 	{
-		Vertex<T, E> ver(node);
-		ver.id = vertexNum;
-		nodeTable.emplace_back(ver);
-		vertexNum ++;
+		std::string v1 = encoder(arr[i]);
+		std::string v2 = encoder(i);
+		printf("(%s, %s),", v1, v2);
+	}
+}
+
+// The main function that constructs Minimum Spanning Tree (MST)
+// using Prim's algorithm
+void PrimMST(ALGraph* graph)
+{
+	int V = graph->vertexSize;// Get the number of vertices in graph
+	std::vector<int> parent(V);   // store constructed MST
+	std::vector<int> key(V);	// Key values used to pick minimum weight edge in cut
+
+	// minHeap represents set E
+	struct MinHeap* minHeap = createMinHeap(V);
+
+	// Initialize min heap with all vertices. Key value of
+	// all vertices (except 0th vertex) is initially infinite
+	for (int v = 1; v < V; ++v)
+	{
+		parent[v] = -1;
+		key[v] = INT_MAX;
+		minHeap->array[v] = newMinHeapNode(v, key[v]);
+		minHeap->pos[v] = v;
 	}
 
-	//step 2 : build edge list
-	for (const auto & edgeInfoArray : edgesInfoArrays)
+	// Make key value of 0th vertex as 0 so that it
+	// is extracted first
+	key[0] = 0;
+	minHeap->array[0] = newMinHeapNode(0, key[0]);
+	minHeap->pos[0] = 0;
+
+	// Initially size of min heap is equal to V
+	minHeap->size = V;
+
+	// In the followin loop, min heap contains all nodes
+	// not yet added to MST.
+	while (!isEmpty(minHeap))
 	{
-		//store the datas
-		E weight;
-		T vertexData, nodeData;
-		std::stringstream w(edgeInfoArray[2]), v(edgeInfoArray[0]), n(edgeInfoArray[1]);
-		w >> weight;
-		v >> vertexData;
-		n >> nodeData;
-		
-		// find id of these datas
-		int id1, id2;
-		int i = 0;
-		for (const auto & node : nodeTable)
+		// Extract the vertex with minimum key value
+		struct MinHeapNode* minHeapNode = extractMin(minHeap);
+		int u = minHeapNode->v; // Store the extracted vertex number
+
+		// Traverse through all adjacent vertices of u (the extracted
+								// vertex) and update their key values
+		struct ALNode* pCrawl = graph->array[u].head;
+		while (pCrawl != NULL)
 		{
-			// 为什么无法访问成员
-			if (vertexData == node.data) id1 = i;
-			if (nodeData == node.data) id2 = i;
-			i++;
+			int v = pCrawl->dest;
+
+			// If v is not yet included in MST and weight of u-v is
+			// less than key value of v, then update key value and
+			// parent of v
+			if (isInMinHeap(minHeap, v) && pCrawl->weight < key[v])
+			{
+				key[v] = pCrawl->weight;
+				parent[v] = u;
+				decreaseKey(minHeap, v, key[v]);
+			}
+			pCrawl = pCrawl->next;
 		}
-
-		//add the edge
-		insertEdge(id1, id2);
 	}
-	
-}
 
-template<typename T, typename E>
-bool ALGraph<T, E>::displayBFS()
-{
-
-}
-
-template<typename T, typename E>
-bool ALGraph<T, E>::primMst(int id)
-{
-
+	displayMSTInfo(parent);
 }
--- Graph/main.cpp
+++ Graph/main.cpp
@@ -1,54 +1,31 @@
-#include<iostream>
-#include<vector>
+#include"Graph.h"
 using namespace std;
-
-void f1(vector<int> vec)
-{
-	for (int i = 0; i < 10; i++)
-	{
-		vec.emplace_back(i + 1);
-	}
-}
-
-void f2(vector<int> & vec)
-{
-	for (int i = 0; i < 10; i++)
-	{
-		vec[i] = i + 1;
-	}
-}
-
-void f3(vector<int> * vec)
-{
-	for (int i = 0; i < 10; i++)
-	{
-		(*vec)[i] = i + 2;
-	}
-}
-
-void showVec(vector<int> vec)
-{
-	for (int i = 0; i < 10; i++)
-	{
-		cout << vec[i] << endl;
-	}
-	cout << "*******************" << endl;
-}
 
 int main()
 {
-	vector<int> vec;
-	for (int i = 0; i < 10; i++)
-	{
-		vec.emplace_back(i);
-	}
-	showVec(vec);
-	f1(vec);
-	showVec(vec);
-	f2(vec);
-	showVec(vec);
-	f3(& vec);
-	showVec(vec);
+	cout << "I'm so sorry that I cannot handle the file I/O in C++" << endl;
+	cout << "This program is just a demo that use the given infomation of the graph" << endl;
+	cout << "The graph info:" << endl;
+	cout << "***********************************" << endl;
+	cout << "Vertex: " << "V1, V2, V3, V4, V5, V6" << endl;
+	cout << "Edges:" << "V1,V2,6;V1,V3,1;V1,V4,5;V2,V3,5;V2,V5,6;V3,V4,5;V3,V5,6;V3,V6,4;V4,V5,2;V5,V6,6;" << endl;
+	cout << "***********************************" << endl;
+	cout << "And here is the result of my MST using Prim" << endl;
+
+	ALGraph * g = creatGraph(6);
+	addEdge(g, 0, 6, 1);
+	addEdge(g, 0, 1, 2);
+	addEdge(g, 0, 5, 3);
+	addEdge(g, 1, 5, 2);
+	addEdge(g, 1, 6, 4);
+	addEdge(g, 2, 5, 3);
+	addEdge(g, 2, 6, 4);
+	addEdge(g, 2, 4, 5);
+	addEdge(g, 3, 2, 4);
+	addEdge(g, 4, 6, 5);
+	
+	PrimMST(g);
+
 	system("pause");
 	return 0;
 }
